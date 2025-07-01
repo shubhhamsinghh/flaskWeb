57c8359405b50b2f3597b43e62e77c24adda4477
Shubham Singh <shubham.singh@EYWA.LOCAL>
2025-07-01 17:24:46 +0530

    Changes in ststic file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from routes.auth import auth_bp #login blueprint
+# from routes.auth import auth_bp #login blueprint
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "my-secrate-key"
-    app.register_blueprint(auth_bp)
+    #app.register_blueprint() #auth_bp
     return app
--- run.py
+++ run.py
@@ -6,3 +6,4 @@ if __name__ == "main":
     app.run(debug=True)
 
 
+ 
