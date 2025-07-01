4f4a0720e5f935b0eba5930f97aab90a23635c3c
Shubham Singh <shubhamproject09882@gmail.com>
2025-07-01 22:16:48 +0530

    add routes and care models

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
 from flask import Flask
-# from routes.auth import auth_bp #login blueprint
+from flask_sqlalchemy import SQLAlchemy
+
+#create DB object globally
+db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "my-secrate-key"
-    #app.register_blueprint() #auth_bp
+    app.config['SECRATE_KEY'] = 'scerate-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///todo.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.__init__(app)
+
+    from app.routes.auth import auth_bp
+    from app.routes.auth import tasks_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(tasks_bp)
+
     return app
---
+++ app/routes/auth.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, render_template, redirect, request, url_for, flash, session
+
+auth_bp = Blueprint('auth',__name__)
+
+USER_CREDENTIALS = {
+    'username' : 'admin',
+    'password' : '123123'
+}
+
+@auth_bp.route('/login', methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == USER_CREDENTIALS['username'] and password == USER_CREDENTIALS['password']:
+            session['user'] = username
+            flash('Login Successfully','success')
+        else:
+            flash('Invalid username or password','danger')
+    return render_template('login.html')
+
+
+@auth_bp.route('/logout')
+def logout():
+    session.pop('user', None)
+    flash('User Logout Successfully','info')
+    return redirect(url_for('login'))
