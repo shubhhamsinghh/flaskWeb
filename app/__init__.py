51cabf03b40b5ee7f9e3d511f3c636ca5bd2f412
Shubham Singh <shubham.singh@EYWA.LOCAL>
2025-07-02 17:06:55 +0530

    update html, css and flash session

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,15 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRATE_KEY'] = 'scerate-key'
+    app.config['SECRET_KEY'] = 'your-development-secret-key'
+    app.config['ENV'] = 'development' 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
     from app.routes.auth import auth_bp
-    from app.routes.auth import tasks_bp
+    from app.routes.tasks import tasks_bp
     app.register_blueprint(auth_bp)
     app.register_blueprint(tasks_bp)
 
