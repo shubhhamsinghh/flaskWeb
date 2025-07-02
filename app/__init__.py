f388011e2eeffda955806f4a4a077f14dd17114a
Shubham Singh <shubham.singh@EYWA.LOCAL>
2025-07-02 15:53:02 +0530

    add routes with def and html pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 #create DB object globally
 db = SQLAlchemy()
 
@@ -8,10 +7,10 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRATE_KEY'] = 'scerate-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///todo.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    db.__init__(app)
+    db.init_app(app)
 
     from app.routes.auth import auth_bp
     from app.routes.auth import tasks_bp
