cd39108f89f05defe56651520010ac2ffa6804cb
Shubham Singh <shubhamproject09882@gmail.com>
2025-07-03 21:11:53 +0530

    Final Commit

--- app/models.py
+++ app/models.py
@@ -4,11 +4,3 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(20), default= "Pending")
-
-
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    status = db.Column(db.Integer, default= 0)
